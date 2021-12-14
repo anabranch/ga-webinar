68d717660c60d1e816fe203c6d0e7c168ebbe673
bill_chambers <bill@anyscale.com>
2021-12-14 15:01:20 -0800

    serve

--- serving.py
+++ serving.py
@@ -8,7 +8,7 @@ app = FastAPI()
 @serve.deployment(route_prefix="/")
 @serve.ingress(app)
 class HelloWorld:
-    @app.get("/")
+    @app.get("/query")
     def hello(self):
         return f"Hello world!"
     
@@ -17,3 +17,13 @@ class HelloWorld:
         return
 
 HelloWorld.deploy()
+
+
+
+# import requests
+# import random
+# CLUSTER_TOKEN="08ac92ec-d056-4127-a5f4-bd821e918133"
+# for i in range(10000):
+#     res = requests.get("https://dashboard-ses-ahkbbyeefqkafvexvr4ms86u.anyscale-internal-hsrczdm-0000.anyscale-test-staging.com/serve/ImageRecognitionService", cookies={"anyscale-token": CLUSTER_TOKEN})
+#     print(random.choice(["airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]))
+#     #print(res.content)
